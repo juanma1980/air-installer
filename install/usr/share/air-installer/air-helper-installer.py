b5031b9755531604414990b360db16d1cbbe1b83
lliurex <juanma1980@gmail.com>
2018-05-21 14:56:45 +0200

    Update icon cache

--- install/usr/share/air-installer/air-helper-installer.py
+++ install/usr/share/air-installer/air-helper-installer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 import  airinstaller.airinstaller as installer
 import sys
-
-print("App: %s"%sys.argv[1])
-print("Ico: %s"%sys.argv[2])
+import subprocess
 
 installer=installer.AirInstaller()
 installer.install(sys.argv[1],sys.argv[2])
+subprocess.check_call(['gtkgtk-update-icon-cache','/usr/share/icons/hicolor/')
+
