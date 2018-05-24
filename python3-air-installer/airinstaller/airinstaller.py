5dd33d61cd1eff5543c3239f3353ba9d2a3afc8a
lliurex <juanma1980@gmail.com>
2018-05-24 14:13:24 +0200

    Install air under sdk when all other attempts fail

--- python3-air-installer/airinstaller/airinstaller.py
+++ python3-air-installer/airinstaller/airinstaller.py
@@ -46,7 +46,6 @@ class AirInstaller():
 		finally:
 			if sw_install_adobe:
 				sw_download=self._install_adobeair()
-#				self._install_adobeair_sdk()
 
 		if sw_download==False:
 			self._debug("Adobeair failed to install")
@@ -60,26 +59,18 @@ class AirInstaller():
 		if self._check_file_is_air(air_file):
 			basedir_name=file_name.replace(".air","")
 			self._debug("Installing %s"%air_file)
-			wrkdir=self.adobeair_folder+basedir_name
-			if os.path.isdir(wrkdir):
-				try:
-					shutil.rmtree(wrkdir)
-				except Exception as e:
-					self._debug(e)
-			try:
-				os.makedirs(wrkdir)
-			except Exception as e:
-				self._debug(e)
 			#Non SDK 
 #			os.system("DISPLAY=:0 /usr/bin/Adobe\ AIR\ Application\ Installer -silent -eulaAccepted -location /opt/AdobeAirApp "+air_file)
 			sw_install_err=self._install_air_package(air_file)
 			if sw_install_err:
 				self._debug("Trying with adobe sdk...")
-				air_file=self._recompile_for_certificate_issue(air_file)
-				self._debug("Installing %s"%air_file)
-				sw_install_err=self._install_air_package(air_file)
+				modified_air_file=self._recompile_for_certificate_issue(air_file)
+				self._debug("Installing %s"%modified_air_file)
+				sw_install_err=self._install_air_package(modified_air_file)
 			if sw_install_err:
-				self._debug("Failed to install")
+				self._debug("Failed to install code: %s"%sw_install_err)
+				if sw_install_err!=0:
+					self._install_air_package_sdk(air_file)
 				self.err_code=1
 
 			if not self.err_code:
@@ -91,7 +82,6 @@ class AirInstaller():
 					self._debug("Installed in %s/%s"%(wrkdir,basedir_name))
 				else:
 					self._debug("%s Not Installed!!!"%(basedir_name))
-			#select_icon
 	#def install
 
 	def _install_air_package(self,air_file):
@@ -103,8 +93,29 @@ class AirInstaller():
 			sw_install_err=True
 			self._debug("Install Error: %s"%e)
 		return sw_install_err
+	#def _install_air_package
 
-	def _generate_desktop(self,file_name,):
+	def _install_air_package_sdk(self,air_file):
+		wrkdir=self.adobeair_folder+basedir_name
+		if os.path.isdir(wrkdir):
+			try:
+				shutil.rmtree(wrkdir)
+			except Exception as e:
+				self._debug(e)
+		try:
+			os.makedirs(wrkdir)
+		except Exception as e:
+			self._debug(e)
+		shutil.copyfile (air_file,wrkdir+"/"+file_name)
+		#Copy icon to hicolor
+		hicolor_icon='/usr/share/icons/hicolor/48x48/apps/%s.png'%basedir_name
+		shutil.copyfile (icon,hicolor_icon)
+
+		self._generate_desktop_sdk(file_name)
+		self._debug("Installed in %s/%s"%(wrkdir,air_file))
+	#def _install_air_package_sdk
+
+	def _generate_desktop(self,file_name):
 		basedir_name=file_name.replace('.air','')
 		desktop="/usr/share/applications/%s.desktop"%basedir_name
 		exec_file=self._get_air_bin_file(basedir_name)
@@ -158,7 +169,25 @@ Categories=Application;Education;Development;ComputerScience;\n\
 							break
 		return(target_bin)
 
-	#Deprecated as we no longer work with adobeair-sdk
+	def _generate_desktop_sdk(self,file_name):
+		basedir_name=file_name.replace('.air','')
+		desktop="/usr/share/applications/%s.desktop"%basedir_name
+		f=open(desktop,'w')
+		f.write("[Desktop Entry]\n\
+Encoding=UTF-8\n\
+Version=1.0\n\
+Type=Application\n\
+Exec=/opt/adobe-air-sdk/adobe-air/adobe-air /opt/adobe-air-sdk/"+basedir_name+"/"+file_name+"\n\
+Icon="+basedir_name+".png\n\
+Terminal=false\n\
+Name="+basedir_name+"\n\
+Comment=Application from AdobeAir "+basedir_name+"\n\
+MimeType=application/x-scratch-project\n\
+Categories=Application;Education;Development;ComputerScience;\n\
+")
+		f.close()
+	#def _generate_desktop_sdk
+
 	def _install_adobeair_sdk(self):
 		self._install_adobeair_depends()
 		self._debug("Installing Adobe Air SDK")
